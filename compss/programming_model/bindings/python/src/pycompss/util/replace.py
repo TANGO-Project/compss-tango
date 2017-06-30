032496f3ee2cd79f14e3830a5cb509baebb63ff9
Jorge Ejarque <jorge.ejarque@bsc.es>
2017-06-30 12:56:27 +0200

    updated and added headers

--- compss/programming_model/bindings/python/src/pycompss/util/replace.py
+++ compss/programming_model/bindings/python/src/pycompss/util/replace.py
@@ -1,5 +1,18 @@
-# SOURCE CODE TAKEN FROM BEN KURTOVIC'S GITHUB REPO replace.py FILE
-# https://gist.github.com/earwig/28a64ffb94d51a608e3d
+#
+#  Copyright 2.1.rc17062-2.1.rc17067 Barcelona Supercomputing Center (www.bsc.es)
+#
+#  Licensed under the Apache License, Version 2.1.rc1706 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.1.rc1706
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
 import ctypes
 from ctypes import pythonapi as api
 import sys
